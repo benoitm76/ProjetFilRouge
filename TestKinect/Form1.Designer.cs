766f1e91285be3c0443ef740830103c4e624cebe
Benoit Mouquet <benoitm76@gmail.com>
2013-02-01 10:38:36 +0100

    Récupération Données Kinect

---
+++ TestKinect/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TestKinect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MainGauche";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 536);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(682, 515);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 37);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Feu !";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ TestKinect/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
+
+namespace TestKinect
+{
+    public partial class Form1 : Form
+    {
+        public KinectInput ki;
+        public Form1()
+        {
+            InitializeComponent();
+            ki = new KinectInput();
+            label4.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ki.playerFire += fireEnnemy;
+            ki.playerMove += move;
+            ki.startFire += startFire;
+            ki.stopFire += stopFire;
+            ki.onContinueFire = true;
+        }
+
+        public void fireEnnemy()
+        {
+            label3.Text += "Feu!";
+        }
+
+        public void startFire()
+        {
+            label4.Show();
+        }
+
+        public void stopFire()
+        {
+            label4.Hide();
+        }
+
+        public void move()
+        {            
+            label1.Text = "X : " + ki.oldPointLeftHand.X + " -- Y : " + ki.oldPointLeftHand.Y + " -- Z : " + ki.oldPointLeftHand.Z;
+            label2.Left = (int)(400 * ki.oldPointLeftHand.X + 400);
+            label2.Top = (int)(600 - (300 * ki.oldPointLeftHand.Y + 300)); 
+        }
+    }
+}
